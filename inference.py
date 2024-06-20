deccd08dfec4a426f7d4851d0cdb886e1636a312
YangQianli92 <108046369+YangQianli92@users.noreply.github.com>
2024-06-20 17:59:41 +0800

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,21 @@
+import os
+
+
+def main():
+  device = torch.device('cuda:0')
+    model = Point_PN_PLM().to(device)
+    model.eval()
+    checkpoint = torch.load("./checkpoint/best_model.pth")
+    model.load_state_dict(checkpoint['model_state_dict'])
+    plm = torch.tensor(
+        np.load('./data/negative.npy')).unsqueeze(0).to(device)
+    point_set = np.loadtxt('./data/negative.txt', delimiter=' ').astype(np.float32)
+    point_set[:, 0:3] = pc_normalize(point_set[:, 0:3])
+    point_set = torch.tensor(point_set).unsqueeze(0).transpose(2, 1).to(device)
+    print(point_set.shape, plm.shape)
+    out = model(point_set, plm)
+    print(out)
+
+
+if __name__ == '__main__':
+    main()
